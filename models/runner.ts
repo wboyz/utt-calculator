ce1e04bb848c5fc9a8ee486bc2b71f4f45a6ebd4
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-03-09 13:56:41 +0100

    feat: add elapsed time calculation

--- models/runner.ts
+++ models/runner.ts
@@ -1,10 +1,22 @@
 export class Runner {
-  constructor(public name: string, public pace: number) {}
+  constructor(public name: string, public pace: number) { }
 
   formattedTime(distance: number) {
     const totalTime = this.pace * distance;
     const hours = Math.floor(totalTime / 60);
     const minutes = totalTime % 60;
-    return `${hours}:${minutes.toFixed(2).toString().padStart(2, '0')}`;
+    return `${hours}:${minutes.toFixed(2).padStart(2, '0')}`;
+  }
+
+  calculateElapsedTime(startingTime: string, distance: number): string {
+    const runningTime = this.formattedTime(distance);
+    const [startingHours, startingMinutes] = startingTime.split(':').map(Number);
+    const [runningHours, runningMinutes] = runningTime.split(':').map(Number);
+
+    const totalMinutes = startingHours * 60 + startingMinutes + (runningHours * 60 + runningMinutes);
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toFixed(2).padStart(2, '0')}`;
   }
 }
