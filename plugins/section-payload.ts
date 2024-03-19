b6851fb8650577daa164b9b9b76bd2075b975973
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-03-19 08:22:16 +0100

    fix: :bug: previous section revivier

--- plugins/section-payload.ts
+++ plugins/section-payload.ts
@@ -7,7 +7,8 @@ export default definePayloadPlugin((nuxtApp) => {
   })
   definePayloadReviver('Section', (value) => {
     const parsed = JSON.parse(value);
+    const previousSection = parsed.previousSection ? new Section(parsed.previousSection.id, parsed.previousSection.distance, parsed.previousSection.from, parsed.previousSection.to, parsed.previousSection.defaultStartTime, parsed.previousSection.previousSection, parsed.previousSection.runner) : undefined;
 
-    return new Section(parsed.id, parsed.distance, parsed.from, parsed.to, parsed.defaultStartTime, parsed.previousSection, parsed.runner)
+    return new Section(parsed.id, parsed.distance, parsed.from, parsed.to, parsed.defaultStartTime, previousSection, parsed.runner)
   })
 })
